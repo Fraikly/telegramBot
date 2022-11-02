fcfc7fe86bc6442d9ac2150382e48b9bdec8d257
Fraikly <Frolova55@outlook.com>
2022-11-02 17:05:29 +0300

    The basis for the bot

---
+++ work_bot.php
@@ -0,0 +1,24 @@
+<?php
+const TOKEN = '5666064256:AAEbqI8sO5H-Mpg0bEVKD6EithD9HMPNbE4';
+const URL = 'https://api.telegram.org/bot' . TOKEN . '/';
+const Method_Update = 'getUpdates';
+const Method_Send = 'sendMessage';
+
+
+    $array =json_decode(file_get_contents(URL . Method_Update), JSON_OBJECT_AS_ARRAY);
+    $last_id_message=$array['result'][count($array['result'])-1]['update_id'];
+    $last_chat_id=$array['result'][count($array['result'])-1]['message']['chat']['id'];
+
+
+function useMethod($method,$options=[]){
+    $url =URL . $method. '?' . http_build_query($options);
+    return  json_decode(file_get_contents($url), JSON_OBJECT_AS_ARRAY);
+
+}
+
+echo '<pre>';
+print_r("Последнее полученное сообщение: \n");
+print_r(useMethod(Method_Update,['offset'=>$last_id_message]));
+print_r("Последнее отправленное сообщение: \n");
+print_r(useMethod(Method_Send,[ 'chat_id' => $last_chat_id,'text' => "Проверка1"]));
+echo '<pre>';
